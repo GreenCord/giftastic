6032a78690ab132e75065f2e2bbe0b44f0c9f7ab
GreenCord <steve@nevizen.com>
2017-11-19 08:51:30 -0500

    Basic HTML and CSS added

---
+++ assets/js/giftastic.js
@@ -0,0 +1,22 @@
+/* Reference divs:
+<div id="buttons"></div>
+
+<div id="gif-display" class="gifs-wrapper">
+	<div class="gif">
+		<img src="" alt="" />
+		<span class="gif-rating">Rated: G</span>
+	</div>
+</div>
+*/
+
+$(document).ready(function(){
+	// create giftastic object to contain everything needed for the program
+	// api url, parameters: api key, search term, limit, rating
+	// initial array of categories
+	// function to get and display buttons for categories array
+	// function to get and add buttons based on user input
+	// click handler to get ajax response from api to get and display gifs
+	// click handler to swap still/playing gif images
+	
+
+});
